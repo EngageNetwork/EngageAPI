32891215b8c7fcc98d9612970f4a7f105dcc53b8
Michael Wang <michael.secure.1@icloud.com>
2021-03-29 13:54:31 -0700

    Updates to Message API system

--- _utils/websockets.js
+++ _utils/websockets.js
@@ -4,6 +4,8 @@ class WebSockets {
 		// On client disconnect
 		client.on("disconnect", () => {
 			this.users = this.users.filter((user) => user.socketId !== client.id);
+			console.log('disconnect');
+			console.log(this.users);
 		});
 
 		// Map identity of user to socket id
@@ -12,17 +14,23 @@ class WebSockets {
 				socketId: client.id,
 				userId: userId,
 			});
+			console.log('identity');
+			console.log(this.users);
 		});
 
 		// Subscribe to chat
 		client.on("subscribe", (chat, otherUserId = "") => {
 			this.subscribeOtherUser(chat, otherUserId);
 			client.join(chat);
+			console.log('subscribe');
+			console.log(this.users);
 		});
 
 		// Unsubscribe from chat
 		client.on("unsubscribe", (chat) => {
 			client.leave(chat);
+			console.log('unsubscribe');
+			console.log(this.users);
 		});
 	}
 	
--- message/chat.model.js
+++ message/chat.model.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema;
 
 const schema = new Schema({
 	userIds: [Schema.Types.ObjectId],
-	type: String,
 	chatInitiator: String
 }, {
 	timestamps: true
