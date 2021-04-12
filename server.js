2dd9889f7da252c865efaceb42ed6ea61750f058
Michael Wang <michael.secure.1@icloud.com>
2021-04-12 00:18:54 -0700

    Updated CORS logic

--- server.js
+++ server.js
@@ -18,8 +18,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-// Allow CORS requests from any origin with proper credentials
-app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+// Allow CORS requests from any origin in list with proper credentials
+const allowedOrigin = ['http://localhost:4200', 'http://engageapp.net']
+app.use(cors({ origin: allowedOrigin, credentials: true }));
 
 // API Routes
 app.use('/accounts', require('./accounts/accounts.controller'));
